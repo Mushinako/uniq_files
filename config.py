b38b6e11cccb4f44a09554fe6bd3d09d1b9990ae
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-11-13 14:58:42 -0800

    After clearance

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+"""
+
+WHITELISTS = {
+    "dirnames": [".git", ".vscode", "site-packages", "__pycache__", "node_modules"],
+    "dirpaths": [],
+    "filenames": [".gitignore", "__init__.py"],
+    "filepaths": [],
+    "fileregexes": [],
+}
