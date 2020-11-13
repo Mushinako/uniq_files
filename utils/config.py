d3054d763e65f2daab772434c0292cc8a0e51159
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-11-13 15:59:01 -0800

    0.2 config

--- utils/config.py
+++ utils/config.py
@@ -1,2 +1,39 @@
 """
 """
+import re
+from pathlib import Path
+from dataclasses import dataclass
+from typing import List, Pattern
+
+# Config to be edited
+_WHITELIST_DIRNAMES = [
+    ".git",
+    ".vscode",
+    "site-packages",
+    "__pycache__",
+    "node_modules",
+]
+_WHITELIST_DIRPATHS = []
+_WHITELIST_FILENAMES = [".gitignore", "__init__.py"]
+_WHITELIST_FILEPATHS = []
+_WHITELIST_FILEREGEXES = []
+
+
+@dataclass
+class _Whitelist:
+    """"""
+
+    dirnames: List[str]
+    dirpaths: List[Path]
+    filenames: List[str]
+    filepaths: List[Path]
+    fileregexes: List[Pattern[str]]
+
+
+WHITELIST = _Whitelist(
+    _WHITELIST_DIRNAMES,
+    [Path(path).resolve() for path in _WHITELIST_DIRPATHS],
+    _WHITELIST_FILENAMES,
+    [Path(path).resolve() for path in _WHITELIST_FILEPATHS],
+    [re.compile(regex) for regex in _WHITELIST_FILEREGEXES],
+)
