5fc7289b70557d31a930c5e25e7ce48dedda22b6
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-11-13 22:51:10 -0800

    JSON indentation

--- utils/json_.py
+++ utils/json_.py
@@ -8,4 +8,4 @@ from typing import List
 def write_json(path: Path, data: List) -> None:
     """"""
     with path.open("w") as json_fp:
-        json.dump(data, json_fp)
+        json.dump(data, json_fp, indent=4)
