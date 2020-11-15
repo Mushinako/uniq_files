e1e3539929a7b9bef52f565f40490eca53fb6abf
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-11-15 14:17:06 -0800

    0.3 db

--- utils/file_handler/inspect_file.py
+++ utils/file_handler/inspect_file.py
@@ -4,10 +4,16 @@ Module: Inspect each file and get information
 Public Classes:
     Path_Type
     File_Props
+
+Public Constants:
+    PATH_FUNCTION_MAP
 """
 from __future__ import annotations
+import pathlib
+import zipfile
 from enum import Enum
 from dataclasses import dataclass
+from typing import Dict, Type
 
 from ..utils.special_types import Union_Path
 
@@ -21,6 +27,12 @@ class Path_Type(Enum):
     ZIPPED = 1
 
 
+PATH_CLASS_MAP: Dict[Path_Type, Type[Union_Path]] = {
+    Path_Type.REGULAR: pathlib.Path,
+    Path_Type.ZIPPED: zipfile.Path,
+}
+
+
 @dataclass
 class File_Props:
     """
@@ -32,7 +44,7 @@ class File_Props:
     """
 
     path: Union_Path
-    path_type: int
+    path_type: Path_Type
     size: int
     last_modified: float
     md5: str
--- utils/storage/db.py
+++ utils/storage/db.py
@@ -6,21 +6,104 @@ Public Functions:
     write_db {Path, list[File_Props] -> None}
 """
 from __future__ import annotations
+import sqlite3
 from pathlib import Path
-from typing import Dict, List
+from contextlib import contextmanager
+from typing import Dict, Generator, List, Tuple
 
 from ..utils.special_types import Union_Path
-from ..file_handler.inspect_file import File_Props
+from ..file_handler.inspect_file import PATH_CLASS_MAP, File_Props, Path_Type
 
 
-def read_db(path: Path) -> Dict[Union_Path, File_Props]:
+_TABLE_NAME = "files"
+_CREATE_TABLE_CMD = f"""
+CREATE TABLE IF NOT EXISTS {_TABLE_NAME} (
+    path TEXT PRIMARY KEY,
+    path_type INTEGER NOT NULL,
+    size TEXT NOT NULL,
+    last_modified TEXT NOT NULL,
+    md5 TEXT NOT NULL,
+    sha1 TEXT NOT NULL
+);
+"""
+_TRUNCATE_TABLE_CMD = f"DELETE FROM {_TABLE_NAME};"
+_SELECT_TABLE_CMD = f"""
+SELECT path, path_type, size, last_modified, md5, sha1
+FROM {_TABLE_NAME};
+"""
+_INSERT_TABLE_CMD = f"""
+INSERT INTO {_TABLE_NAME} (path, path_type, size, last_modified, md5, sha1)
+VALUES (?, ?, ?, ?, ?, ?);
+"""
+
+
+def read_db(db_path: Path) -> Dict[Union_Path, File_Props]:
     """
     Read database at given path and returns data in a dictionary
 
     Args:
-        path {Path}
+        db_path {Path}: DB file path
+
+    Returns:
+        {dict[Union_Path, File_Props]}: Path-file property mapping
     """
+    files_props: Dict[Union_Path, File_Props] = {}
+    with _open_db(db_path) as con:
+        with con:
+            con.execute(_CREATE_TABLE_CMD)
+        cursor = con.execute(_SELECT_TABLE_CMD)
+        data: List[Tuple[str, int, str, str, str, str]] = cursor.fetchall()
+        for path_str, path_type_int, size_str, last_modified_str, md5, sha1 in data:
+            path_type = Path_Type(path_type_int)
+            Path_Class = PATH_CLASS_MAP[path_type]
+            path: Union_Path = Path_Class(path_str)
+            files_props[path] = File_Props(
+                path, path_type, int(size_str), int(last_modified_str), md5, sha1
+            )
+    return files_props
 
 
-def write_db(path: Path, files_props: List[File_Props]) -> None:
-    """"""
+def write_db(db_path: Path, files_props: List[File_Props]) -> None:
+    """
+    Write database at given path
+
+    Args:
+        db_path     {Path}            : DB file path
+        files_props {list[File_Props]}: Stuff to be written to the database
+    """
+    with _open_db(db_path) as con:
+        with con:
+            con.execute(_TRUNCATE_TABLE_CMD)
+        with con:
+            con.executemany(
+                _INSERT_TABLE_CMD,
+                (
+                    (
+                        str(fp.path),
+                        fp.path_type.value,
+                        str(fp.size),
+                        str(fp.last_modified),
+                        fp.md5,
+                        fp.sha1,
+                    )
+                    for fp in files_props
+                ),
+            )
+
+
+@contextmanager
+def _open_db(db_path: Path) -> Generator[sqlite3.Connection, None, None]:
+    """
+    Context manager for the sqlite3 database
+
+    Args:
+        db_path {Path}: DB file path
+
+    Yields:
+        {sqlite3.Connection}: DB connection
+    """
+    con = sqlite3.connect(db_path)
+    try:
+        yield con
+    finally:
+        con.close()
