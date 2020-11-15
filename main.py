8f128097d27100a73d5233ad29faa4fcd9fc51dc
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-11-15 11:00:43 -0800

    0.1 restructure

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
-from utils.parse_argv import parse_argv
-from utils.db import read_database, write_database
-from utils.walk_directory import iter_walk, iter_walk_size
-from utils.inspect_file import check_files
-from utils.json_ import write_json
+from __future__ import annotations
+
+from utils.argv.parse_argv import parse_argv
 
 
-def main() -> None:
+def main():
+    """"""
     # Parse command-line arguments
     args = parse_argv()
     # Read DB
-    files_data = read_database(args.file_database_path)
-    # Walk directory recursively
-    print("Calculating size...")
-    total_size = iter_walk_size(args.base_path)
-    walker = iter_walk(args.base_path)
-    output_json_list, all_files = check_files(walker, files_data, total_size)
-    # Write JSON
-    write_json(args.dup_json_path, output_json_list)
-    # Write DB
-    write_database(args.file_database_path, all_files)
 
 
 if __name__ == "__main__":
---
+++ utils/argv/parse_argv.py
@@ -0,0 +1,67 @@
+"""
+Module: Parsing command-line arguments
+
+Public Functions:
+ - parse_argv {None -> _Args}
+"""
+from __future__ import annotations
+from pathlib import Path
+from argparse import ArgumentParser, Namespace
+
+
+def parse_argv() -> _Args:
+    """
+    Parse command-line arguments
+
+    Returns:
+        {_Args}: Namespace object for all arguments
+    """
+    parser = ArgumentParser(description="Check file duplicates under some base path")
+    parser.add_argument("base_path", type=_full_path, help="Base path")
+    parser.add_argument("json_path", type=_full_path, help="Duplication JSON path")
+    parser.add_argument("db_path", type=_full_path, help="File database path")
+    args: _Args = parser.parse_args()
+
+    _prepare_argv(args)
+
+    return args
+
+
+class _Args(Namespace):
+    """
+    Command-line arguments object, for type annotations only
+    """
+
+    base_path: Path
+    json_path: Path
+    db_path: Path
+
+
+def _full_path(arg: str) -> Path:
+    """
+    Helper function for resolving all the paths to absolute Path objects
+
+    Args:
+        arg {str}: Command-line argument
+
+    Returns:
+        {Path}: Absolute Path object
+    """
+    return Path(arg).resolve()
+
+
+def _prepare_argv(args: _Args) -> None:
+    """
+    Check arguments validity and make preparations
+
+    args {_Args}: Parsed command-line args
+    """
+    if not args.base_path.is_dir():
+        raise FileNotFoundError(f"`base_path` not a directory: {args.base_path}")
+    if args.json_path.exists() and not args.json_path.is_file():
+        raise FileNotFoundError(f"`dup_json_path` not a file: {args.json_path}")
+    if args.db_path.exists() and not args.db_path.is_file():
+        raise FileNotFoundError(f"`file_database_path` not a file: {args.db_path}")
+
+    args.json_path.parent.mkdir(parents=True, exist_ok=True)
+    args.db_path.parent.mkdir(parents=True, exist_ok=True)
