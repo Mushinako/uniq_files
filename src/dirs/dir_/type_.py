d54a8a158c537305105e7c6efdcda64ebebf2b40
Mushinako <8977737+Mushinako@users.noreply.github.com>
2020-12-31 11:40:38 -0800

    Correct path class

--- src/dirs/dir_/type_.py
+++ src/dirs/dir_/type_.py
@@ -7,7 +7,9 @@ Public Classes:
 
 from __future__ import annotations
 
-from pathlib import Path
+import os
+from pathlib import Path, WindowsPath, PosixPath
+from typing import Any, Union
 
 
 class DirPath(Path):
@@ -15,8 +17,21 @@ class DirPath(Path):
     Similar to pathlib.Path, but works with context manager
     """
 
+    def __new__(cls, *args: Union[str, os.PathLike[str]], **kwargs: Any) -> DirPath:
+        if cls is DirPath:
+            cls = _WindowsDirPath if os.name == "nt" else _PosixDirPath
+        return super().__new__(cls, *args, **kwargs)
+
     def __enter__(self) -> DirPath:
         return self
 
     def __exit__(self, *_) -> None:
         pass
+
+
+class _WindowsDirPath(DirPath, WindowsPath):
+    pass
+
+
+class _PosixDirPath(DirPath, PosixPath):
+    pass
